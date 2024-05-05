ea5a1e8fe91d942d125b7c7b1cb4dc27fff289b4
Marcin Cupryś <162025701+MarcinCuprys13@users.noreply.github.com>
2024-05-05 22:14:14 +0200

    azurerm-blob-storage

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,49 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+  default     = "example-resources"  
+}
+
+variable "resource_group_location" {
+  description = "Lokalizacja grupy zasobów"
+  default     = "West Europe"  
+}
+
+variable "storage_account_name" {
+  description = "Nazwa konta magazynu"
+  default     = "examplestoracc"  
+}
+
+variable "storage_account_tier" {
+  description = "Typ konta magazynu"
+  default     = "Standard" 
+}
+
+variable "storage_account_replication_type" {
+  description = "Typ replikacji konta magazynu"
+  default     = "LRS" 
+}
+
+variable "storage_container_name" {
+  description = "Nazwa kontenera magazynu"
+  default     = "content"  
+}
+
+variable "storage_container_access_type" {
+    description     = "Poziom dostępu do kontenera"
+    default         = "private" 
+}
+
+variable "blob_name" {
+  description = "Nazwa pliku BLOB"
+  default     = "my-awesome-content.zip"  
+}
+
+variable "blob_type" {
+    description     = "Typ blobu"
+    default         = "Block"
+}
+
+variable "blob_source" {
+  description = "Ścieżka do lokalnego pliku źródłowego"
+  default     = "some-local-file.zip"  
+}
